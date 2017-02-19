2825ac4b4aa6a3fd365c5953810bd2f9c2bbc88a
Marius Gavrilescu <marius@ieval.ro>
2017-02-19 23:24:47 +0000

    Partial implementation of Battleships protocol

---
+++ backend/protocol_battleships.c
@@ -0,0 +1,220 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#define LWS_DLL
+#include <jansson.h>
+#include "libwebsockets/lib/libwebsockets.h"
+#include "game_logic.h"
+
+struct per_session_data__battleships {
+	json_t *pending_replies[100];
+	int pending_replies_count;
+};
+
+struct notification_user_data {
+	struct per_session_data__battleships *pss;
+	json_t *id;
+};
+
+/** Like json_pack(fmt, ...) in Jansson, but halts program on error */
+static json_t *croaking_json_pack(const char* fmt, ...) {
+	json_error_t err;
+	va_list ap;
+	json_t *result;
+
+	va_start(ap, fmt);
+	result = json_vpack_ex(&err, 0, fmt, ap);
+	va_end(ap);
+
+	if(result == NULL) {
+		lwsl_err("Error in json_vpack_ex: %s", err.text);
+		exit(1);
+	}
+	return result;
+}
+
+static void send_json_rpc(struct lws *wsi, struct per_session_data__battleships *pss, json_t *payload) {
+	assert(payload != NULL);
+	assert(json_object_set_new(payload, "jsonrpc", json_string("2.0")) == 0);
+	pss->pending_replies[pss->pending_replies_count++] = payload;
+	lws_callback_on_writable(wsi);
+}
+
+static json_t *make_json_rpc_error(json_t *id, int code, const char* message, json_t *data) {
+	return croaking_json_pack("{s: O?, s: {s: i, s: s, s: o?}}", "id", id, "error", "code", code, "message", message, "data", data);
+}
+
+static json_t *make_json_rpc_result(json_t *id, json_t *result) {
+	return croaking_json_pack("{s: O, s: o}", "id", id, "result", result);
+}
+
+#define DIE(...) send_json_rpc(wsi, pss, make_json_rpc_error(__VA_ARGS__))
+#define DIE_IF(condition, ...) if(condition) { \
+	DIE(__VA_ARGS__);                          \
+	return;                                    \
+	}
+#define DIE_ON_ERROR(error) DIE_IF(error < 0, id, error, error_to_string(error), NULL)
+#define EXPECT_PARAMS(N) DIE_IF(paramN != N, id, -32602, "Invalid params", json_string("Expected " #N " param(s)"))
+#define REPLY(result) send_json_rpc(wsi, pss, make_json_rpc_result(id, result))
+
+#define PARAM_ID json_integer_value(json_array_get(params, 0))
+
+static void handle_request(struct lws *wsi, struct per_session_data__battleships *pss, json_t *request) {
+	int paramN, result, x, y;
+	char *jsonrpc = "", *method = "";
+	json_t *params = NULL, *id = NULL;
+	json_error_t error;
+	grid grd;
+
+	new_game_result ng_result;
+	get_game_end_result gge_result;
+	plyr_id pid;
+	plyr_state pstate;
+
+	result = json_unpack_ex(request, &error, JSON_STRICT, "{s: s, s: s, s: o, s?: o}",
+	               "jsonrpc", &jsonrpc, "method", &method, "id", &id, "params", &params);
+
+	DIE_IF(result, id, -32600, "Invalid Request", json_string(error.text));
+	DIE_IF(strcmp(jsonrpc, "2.0"), id, -32600, "Invalid Request", json_string("JSON-RPC version not 2.0"));
+
+	paramN = json_array_size(params);
+	if(strcmp(method, "newGame") == 0) {
+		EXPECT_PARAMS(0);
+		ng_result = bship_logic_new_game();
+		REPLY(croaking_json_pack("[ii]", ng_result.gid, ng_result.pid));
+	} else if(strcmp(method, "joinGame") == 0) {
+		EXPECT_PARAMS(1);
+		pid = bship_logic_join_game((game_id)PARAM_ID);
+		DIE_ON_ERROR(pid);
+		REPLY(json_integer(pid));
+	} else if(strcmp(method, "submitGrid") == 0) {
+		EXPECT_PARAMS(2);
+		grd = NULL; /* XXX: currently ignoring grid parameter */
+		result = bship_logic_submit_grid((plyr_id)PARAM_ID, grd);
+		DIE_ON_ERROR(result);
+		REPLY(json_null());
+	} else if(strcmp(method, "bombPosition") == 0) {
+		EXPECT_PARAMS(3);
+		pid = (plyr_id) PARAM_ID;
+		x = json_integer_value(json_array_get(params, 1));
+		y = json_integer_value(json_array_get(params, 2));
+		result = bship_logic_bomb_position(pid, x, y);
+		DIE_ON_ERROR(result);
+		REPLY(json_boolean(result));
+	} else if(strcmp(method, "getGameEnd") == 0) {
+		EXPECT_PARAMS(1);
+		gge_result = bship_logic_get_game_end((plyr_id)PARAM_ID);
+		/* Can't error, returns game_over = false on error */
+		/* The below is wrong because grid is not a json_t. XXX: once grid means something, encode it properly */
+		REPLY(croaking_json_pack("{s: b, s: b, s: n}", "game_over", gge_result.game_over, "won", gge_result.won, "grid"));
+	} else if(strcmp(method, "waitForPlayer") == 0) {
+		EXPECT_PARAMS(1);
+		result = bship_logic_request_notify((plyr_id)PARAM_ID, SUBMIT_GRID, pss);
+		DIE_ON_ERROR(result);
+	} else if(strcmp(method, "waitForSubmit") == 0) {
+		EXPECT_PARAMS(1);
+		result = bship_logic_request_notify((plyr_id)PARAM_ID, BOMB, pss);
+		DIE_ON_ERROR(result);
+	} else if(strcmp(method, "waitForMove") == 0) {
+		EXPECT_PARAMS(1);
+		result = bship_logic_request_notify((plyr_id)PARAM_ID, BOMB, pss);
+		DIE_ON_ERROR(result);
+	} else {
+		DIE(id, -32601, "Method not found", NULL);
+	}
+	return;
+}
+
+static int callback_bship(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len) {
+	struct per_session_data__battleships *pss =
+		(struct per_session_data__battleships *) user;
+	char buf[LWS_PRE + 1024];
+	char *p = &buf[LWS_PRE], *q;
+	int i, m, n;
+	json_t *request;
+	json_error_t error;
+
+	switch(reason) {
+	case LWS_CALLBACK_PROTOCOL_INIT:
+		lwsl_notice("Initialized battleships for a vhost");
+		break;
+
+	case LWS_CALLBACK_ESTABLISHED:
+		pss->pending_replies_count = 0;
+		break;
+
+	case LWS_CALLBACK_CLOSED:
+		for(i = 0; i < pss->pending_replies_count ; i++)
+			json_decref(pss->pending_replies[i]);
+		break;
+
+	case LWS_CALLBACK_SERVER_WRITEABLE:
+		/* If multiple replies available, send them in reverse order
+		 * for simplicty */
+		if(pss->pending_replies_count == 0)
+			break;
+		q = json_dumps(pss->pending_replies[--pss->pending_replies_count], 0);
+		json_decref(pss->pending_replies[pss->pending_replies_count]);
+		strncpy(p, q, 1024);
+		free(q);
+		n = strlen(p);
+		m = lws_write(wsi, (unsigned char*)p, n, LWS_WRITE_TEXT);
+
+		if (m < n) {
+			lwsl_err("ERROR %d writing to socket\n", n);
+			return -1;
+		}
+
+		if(pss->pending_replies_count)
+			lws_callback_on_writable(wsi);
+		break;
+
+	case LWS_CALLBACK_RECEIVE:
+		request = json_loads(in, 0, &error);
+		if(request == NULL) {
+			lwsl_err("ERROR decoding response: '%s'\n", error.text);
+			DIE(NULL, -32700, "JSON parse error", json_string(error.text));
+			return 0;
+		}
+
+		handle_request(wsi, pss, request);
+		json_decref(request);
+		break;
+	}
+	return 0;
+}
+
+void bship_logic_notification(plyr_id id, plyr_state state, void *user) {
+	struct per_session_data__battleships *pss = (struct per_session_data__battleships*) user;
+	/* XXX: do nothing yet */
+}
+
+static const struct lws_protocols protocols[] = {
+	{
+		"battleships-protocol",
+		callback_bship,
+		(sizeof (struct per_session_data__battleships)),
+		1024,
+	},
+};
+
+LWS_VISIBLE int
+init_protocol_battleships(struct lws_context *context, struct lws_plugin_capability *c) {
+	if (c->api_magic != LWS_PLUGIN_API_MAGIC) {
+		lwsl_err("Plugin API %d, library API %d", LWS_PLUGIN_API_MAGIC,
+			 c->api_magic);
+		return 1;
+	}
+
+	c->protocols = protocols;
+	c->count_protocols = 1;
+	c->extensions = NULL;
+	c->count_extensions = 0;
+	return 0;
+}
+
+LWS_VISIBLE int
+destroy_protocol_battleships(struct lws_context *context) {
+	return 0;
+}
