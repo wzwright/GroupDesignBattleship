c228c21d516455e9a1d4f08dbc07953fc2ffd94b
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-03-14 14:16:17 +0000

    Added comment explaining dummy_functions.c

--- backend/dummy_functions.c
+++ backend/dummy_functions.c
@@ -1,3 +1,9 @@
+/* This file contains functions used by the unit tests of
+ * bship_logic.so. This is needed since these symbols are not defined
+ * (but are used) in bship_logic.pyx, so the Python module .so it
+ * produces is not loadable as-is: we need to provide the symbols.
+ */
+
 void bship_logic_notification(int pid, int state, void *data, int success) {
 	
 }
