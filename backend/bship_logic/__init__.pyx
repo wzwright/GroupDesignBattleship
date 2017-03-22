5511670eee7c9f2cb87dbaf94ef725f22ee86d71
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2017-03-22 19:08:30 +0000

    Wrote example AI

--- backend/bship_logic/__init__.pyx
+++ backend/bship_logic/__init__.pyx
@@ -1,3 +1,5 @@
 include "logic.py"
 include "c_api.pyx"
 include "test_api.pyx"
+include "util.pyx"
+include "ai.pyx"
---
+++ backend/bship_logic/ai.pyx
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+class AIPlayer(Player, ABC):
+    def __init__(self, pid):
+        super(AIPlayer, self).__init__(pid, "AI Player")
+        
+    def set_state(self, state):
+        super(AIPlayer, self).set_state(state)
+        # in addition to setting the state, we also actually do the
+        # action our new state allows us to perform
+        if state == PlayerState.SUBMIT_GRID:
+            self.ai_submit_grid()
+        elif state == PlayerState.BOMB:
+            self.ai_bomb()
+        elif state == PlayerState.GAME_OVER:
+            self.ai_cleanup()
+
+    @abstractmethod
+    def ai_submit_grid(self):
+        pass
+
+    @abstractmethod
+    def ai_bomb(self):
+        pass
+
+    @abstractmethod
+    def ai_cleanup(self):
+        pass
+
+class ExampleAIPlayer(AIPlayer):
+    "Example AI that just bombs (0,0) repeatedly"
+    def ai_submit_grid(self):
+        # A real AI would pick a harder grid here
+        mygrid = [[0,0,1,0]
+                 ,[0,1,2,1]
+                 ,[0,2,2,2]
+                 ,[0,3,3,3]
+                 ,[0,4,4,4]]
+        bship_logic_submit_grid(self.pid, python_to_grid(mygrid))
+
+    def ai_bomb(self):
+        # bombs (0,0) every time
+        bship_logic_bomb_position(self.pid, 0, 0)
+
+    def ai_cleanup(self):
+        pass
+
