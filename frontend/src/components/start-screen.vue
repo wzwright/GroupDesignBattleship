e7f18cd69c528dfef662cad76746960331ff5643
Winston Wright <winstonwright88@gmail.com>
2017-04-07 12:51:05 -0700

    Revert "Allows random games from frontend"
    This reverts commit 0fa97e6ce1b8082dcb22fcd5d01aff71cb252691.

--- frontend/src/components/start-screen.vue
+++ frontend/src/components/start-screen.vue
@@ -9,7 +9,6 @@
     ></textInput>
     <button v-on:click="newGame">New Game</button>
     <button v-on:click="joinGame">Join Existing Game</button>
-    <button v-on:click="joinRandomGame" ref="random">Join Random Game</button>
   </div>
 </template>
 
@@ -45,20 +44,6 @@ export default {
       this.$store.commit('setPlayerNickname', this.nickname)
       this.$emit('changeScreen', 'joinGame')
     },
-    joinRandomGame() {
-      this.$store.commit('setPlayerNickname', this.nickname)
-      this.$store.dispatch('joinRandomGame', {
-        okCallback: () => {
-          this.$refs.random.innerText="Waiting..."
-          this.$store.dispatch('waitForPlayer', {
-            okCallback: () => {
-              this.$store.dispatch('getOpponentNickname', {})
-              this.$emit('changeScreen', 'noOverlay')
-            },
-          })
-        },
-      })
-    }
   },
 }
 </script>
